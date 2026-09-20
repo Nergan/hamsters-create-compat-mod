3a5ddbbda5a8d9ac34f5fe48c92af9c2b5e460e5
Nergan <mak.kriuchkov@yandex.ru>
2026-09-20 23:18:50 +0700

    fix game crash 2

--- src/main/java/com/hamsterscreate/compat/client/CompatClientEvents.java
+++ src/main/java/com/hamsterscreate/compat/client/CompatClientEvents.java
@@ -2,9 +2,10 @@ package com.hamsterscreate.compat.client;
 
 import com.hamsterscreate.compat.HamstersCreateCompat;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(
     modid = HamstersCreateCompat.MOD_ID,
@@ -15,8 +16,8 @@ public final class CompatClientEvents {
     private CompatClientEvents() {
     }
 
-    @SubscribeEvent
-    public static void onClientSetup(FMLClientSetupEvent event) {
-        event.enqueueWork(CompatClient::replaceWheelRenderer);
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        CompatClient.replaceWheelRenderer(event);
     }
 }
--- src/main/kotlin/com/hamsterscreate/compat/client/CompatClient.kt
+++ src/main/kotlin/com/hamsterscreate/compat/client/CompatClient.kt
@@ -2,14 +2,19 @@ package com.hamsterscreate.compat.client
 
 import com.hamsterscreate.compat.kinetics.HamsterWheelKineticBlockEntity
 import com.starfish_studios.hamsters.registry.HamstersBlockEntities
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderers
 import net.minecraft.world.level.block.entity.BlockEntityType
+import net.minecraftforge.client.event.EntityRenderersEvent
 
 object CompatClient {
+    private val renderer = HamsterWheelKineticRenderer()
+
     @JvmStatic
-    fun replaceWheelRenderer() {
+    fun wheelRenderer(): HamsterWheelKineticRenderer = renderer
+
+    @JvmStatic
+    fun replaceWheelRenderer(event: EntityRenderersEvent.RegisterRenderers) {
         @Suppress("UNCHECKED_CAST")
         val type = HamstersBlockEntities.HAMSTER_WHEEL.get() as BlockEntityType<HamsterWheelKineticBlockEntity>
-        BlockEntityRenderers.register(type) { HamsterWheelKineticRenderer() }
+        event.registerBlockEntityRenderer(type) { renderer }
     }
 }
